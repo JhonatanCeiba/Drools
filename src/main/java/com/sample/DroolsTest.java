bd9375ea5640b4096dabf02262d9133ff3524d2c
jhonatan.giraldo <jhonatan.giraldo@jhonatan-giraldo.ceiba.local>
2016-07-27 17:06:26 -0500

    Segundo Commit. Desacoplando. Se saca la clase Message de la DroolsTest(que es la principal)

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -12,6 +12,8 @@ import org.drools.logger.KnowledgeRuntimeLogger;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 
+
+
 /**
  * This is a sample class to launch a rule.
  */
@@ -51,33 +53,6 @@ public class DroolsTest {
         return kbase;
     }
 
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int TRYING = 1;
-        public static final int GOODBYE = 2;
-        public static final int FINISH = 3;
-        
-
-        private String message;
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
+    
 
 }
